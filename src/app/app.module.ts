09286ea8477df663a8c6cc812fb880c0f1901fc1
FherPie <panaonchoatu@hotmail.com>
2021-03-28 11:19:48 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PantallaNosotrosComponent } from './pantalla-nosotros/pantalla-nosotros.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { RegistroComponent } from './auth/registro/registro.component';
+import { LoginComponent } from './auth/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PasswordForgotComponent } from './auth/password-forgot/password-forgot.component';
+import { OlvidoContrasenaComponent } from './auth/olvido-contrasena/olvido-contrasena.component';
+import { ListaFundacionesComponent } from './lista-fundaciones/lista-fundaciones.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PantallaNosotrosComponent,
+    InicioComponent,
+    RegistroComponent,
+    LoginComponent,
+    PasswordForgotComponent,
+    OlvidoContrasenaComponent,
+    ListaFundacionesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule, 
+    RouterModule.forRoot([{path: 'home-page/:id', component: AppComponent},
+    {path: 'nosotros-page/:id', component: PantallaNosotrosComponent},
+    {path: 'inicio-page', component: InicioComponent},
+    {path: 'registro-page', component: RegistroComponent},
+    {path: 'olvido-contrasena', component: OlvidoContrasenaComponent},
+    {path: 'login-page', component: LoginComponent},
+    {path: 'fundaciones-page', component: ListaFundacionesComponent},
+    {path: 'p', redirectTo:'registro-page', pathMatch: 'full'},
+    ]),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/lista-fundaciones/lista-fundaciones.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-fundaciones',
+  templateUrl: './lista-fundaciones.component.html',
+  styleUrls: ['./lista-fundaciones.component.css']
+})
+export class ListaFundacionesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
